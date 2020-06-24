423c7545782d8a0aff822ee4f77c5a05d718cb6e
Dominik Schilling <dominikschilling+git@gmail.com>
2020-06-24 20:00:20 +0200

    Remove checkout command for now

--- packages/wordpress-plugin-boilerplate/lib/cli.js
+++ packages/wordpress-plugin-boilerplate/lib/cli.js
@@ -2,22 +2,13 @@
 
 const program = require( 'commander' );
 const replace = require( './commands/replace' );
-const checkout = require( './commands/checkout' );
 
 program.version( require( '../package.json' ).version );
-
-program
-	.command( 'checkout' )
-	.alias( 'co' )
-	.description( 'clone the repository to your local folder' )
-	.action( checkout );
 
 program
 	.command( 'replace' )
 	.option( '-n, --dry-run', 'run without actually making replacements' )
 	.description( 'rename plugin name and other variables' )
 	.action( replace );
-
-program.command( 'create' ).description( 'create a new plugin' );
 
 module.exports = program;
