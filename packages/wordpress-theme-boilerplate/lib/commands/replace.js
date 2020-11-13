061e9af2cd3040cd26682aac29c18b5f7d81b30f
Ulrich Pogson <grapplerulrich@gmail.com>
2020-11-13 17:25:15 +0100

    Create command for theme boilerplate
    https://github.com/wearerequired/default-wordpress-theme/issues/44

---
+++ packages/wordpress-theme-boilerplate/lib/commands/replace.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const { promisify } = require( 'util' );
+const fs = require( 'fs' ).promises;
+const inquirer = require( 'inquirer' );
+const replace = require( 'replace-in-file' );
+const { pascalCase, camelCase, paramCase, snakeCase } = require( 'change-case' );
+const rimraf = promisify( require( 'rimraf' ) );
+const { log, format } = require( '../logger' );
+const { validateSlug, validatePHPNamespace, validateNotEmpty } = require( '../validation' );
+const { runStep } = require( '../utils' );
+
+const WORKING_DIR = process.cwd();
+
+async function replaceFiles() {
+	try {
+		await fs.access( WORKING_DIR + '/style.css' );
+	} catch ( e ) {
+		log( format.error( WORKING_DIR + '/style.css not found' ) );
+		process.exit();
+	}
+
+	const {
+		ThemeName,
+		themeDescription,
+		themeSlug,
+		phpNamespace,
+		githubSlug,
+	} = await inquirer.prompt( [
+		{
+			type: 'input',
+			name: 'ThemeName',
+			default: 'My Theme',
+			message: 'Enter the name of the theme:',
+			validate: validateNotEmpty,
+		},
+		{
+			type: 'input',
+			name: 'themeDescription',
+			default: '',
+			message: 'Enter the description of the theme:',
+		},
+		{
+			type: 'input',
+			name: 'themeSlug',
+			default: ( answers ) => paramCase( answers.ThemeName ),
+			message: 'Enter the slug of the theme:',
+			validate: validateSlug,
+		},
+		{
+			type: 'input',
+			name: 'phpNamespace',
+			default: ( answers ) => 'Required\\' + pascalCase( answers.themeSlug ),
+			message: 'Enter the PHP namespace of the theme:',
+			validate: validatePHPNamespace,
+		},
+		{
+			type: 'input',
+			name: 'githubSlug',
+			default: ( answers ) => answers.themeSlug,
+			message: 'Enter the slug of the GitHub repo:',
+			validate: validateSlug,
+		},
+	] );
+
+	log();
+
+	const { isReady } = await inquirer.prompt( [
+		{
+			type: 'expand',
+			name: 'isReady',
+			default: 2, // default to help in order to avoid clicking straight through
+			choices: [
+				{ key: 'y', name: 'Yes', value: true },
+				{ key: 'n', name: 'No', value: false },
+			],
+			message:
+				'Start rewriting the files for ' + ThemeName + ' with slug ' + themeSlug + '?',
+		},
+	] );
+
+	if ( ! isReady ) {
+		log( format.error( '\n\nAborting.' ) );
+		process.exit();
+	}
+
+	log();
+
+	await runStep( 'Renaming main theme file', 'Could not rename file.', async () => {
+		await fs.rename(
+			WORKING_DIR + '/style.css',
+			WORKING_DIR + '/' + themeSlug + '.php'
+		);
+	} );
+
+	await runStep( 'Updating README.md file', 'Could not update README.md.', async () => {
+		await fs.writeFile( WORKING_DIR + '/README.md', '# ' + ThemeName + '\n' );
+	} );
+
+	await runStep( 'Renaming theme files', 'Could not rename files.', async () => {
+		const replacementOptions = {
+			allowEmptyPaths: true,
+			files: [
+				themeDir + '/README.md',
+				themeDir + '/composer.json',
+				themeDir + '/package.json',
+				themeDir + '/phpcs.xml.dist',
+				themeDir + '/style.css',
+				themeDir + '/*.php',
+				themeDir + '/inc/*.php',
+				themeDir + '/template-parts/*.php',
+			],
+			from: [
+				/Theme Name([^:])/g, // Ignore the colon so that in "Theme Name: Theme Name" only the second is replaced.
+				/Required\\ThemeName/g,
+				/Required\\\\ThemeName/g,
+				/theme-name/g,
+				/theme_name/g,
+				/ThemeName/g,
+				/Theme description\./g,
+				/wordpress-theme-boilerplate/g,
+			],
+			to: [
+				ThemeName + '$1',
+				phpNamespace,
+				phpNamespace.replace( /\\/g, '\\\\' ),
+				themeSlug,
+				snakeCase( themeSlug ),
+				camelCase( themeSlug ),
+				themeDescription,
+				githubSlug,
+			],
+		};
+
+		await replace( replacementOptions );
+	} );
+
+	log( format.success( '\n✅  Done!' ) );
+}
+
+module.exports = replaceFiles;
