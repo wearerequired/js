36118c3f7d371d8da66a80e66b29d98af225b034
Ulrich Pogson <grapplerulrich@gmail.com>
2021-02-17 09:29:23 +0100

    Create command for theme boilerplate (#11)
    * Create command for theme boilerplate
    
    https://github.com/wearerequired/default-wordpress-theme/issues/44
    
    * Theme: Add project name support
    
    Increase time for GitHub to create repo
    
    * Theme: fix renaming & disable issues, wiki, projects
    
    * Remove project references and change default Namespace
    
    * Use specific store key for GitHub token
    
    * Fix linting issues
    
    * Don't disable issues by default
    
    * Remove todo comment
    
    * Remove unused dependencies
    
    * Use correct variable
    
    * Don't replace readme file
    
    * Don't try to rename style.css
    
    * Use correct casing for variable name
    
    * Update changelog
    
    * Fix default namespace
    
    Co-authored-by: Dominik Schilling <dominikschilling+git@gmail.com>

---
+++ packages/wordpress-theme-boilerplate/README.md
@@ -0,0 +1,11 @@
+# @wearerequired/wordpress-theme-boilerplate
+
+Command for bootstrapping a new WordPress theme based on the [WordPress Theme Boilerplate](https://github.com/wearerequired/wordpress-theme-boilerplate). It offers a modern build setup and generates PHP, JS, CSS code, and everything else you need to start the theme.
+
+## Usage
+
+```bash
+npx @wearerequired/wordpress-theme-boilerplate@latest create
+```
+
+[![asciicast](https://asciinema.org/a/hGqJx9FeQrgc7pjwnblBdkiq3.svg)](https://asciinema.org/a/hGqJx9FeQrgc7pjwnblBdkiq3?speed=5&autoplay=1)
---
+++ packages/wordpress-theme-boilerplate/lib/commands/replace.js
@@ -0,0 +1,128 @@
+'use strict';
+
+const fs = require( 'fs' ).promises;
+const inquirer = require( 'inquirer' );
+const replace = require( 'replace-in-file' );
+const { pascalCase, camelCase, paramCase, snakeCase } = require( 'change-case' );
+const { log, format } = require( '../logger' );
+const { validateSlug, validatePHPNamespace, validateNotEmpty } = require( '../validation' );
+const { runStep } = require( '../utils' );
+
+const WORKING_DIR = process.cwd();
+
+async function replaceFiles() {
+	try {
+		await fs.access( WORKING_DIR + '/style.css' );
+	} catch ( e ) {
+		log( format.error( WORKING_DIR + '/style.css not found' ) );
+		process.exit();
+	}
+
+	const {
+		themeName,
+		themeDescription,
+		themeSlug,
+		phpNamespace,
+		githubSlug,
+	} = await inquirer.prompt( [
+		{
+			type: 'input',
+			name: 'themeName',
+			default: 'My Theme',
+			message: 'Enter the name of the theme:',
+			validate: validateNotEmpty,
+		},
+		{
+			type: 'input',
+			name: 'themeDescription',
+			default: '',
+			message: 'Enter the description of the theme:',
+		},
+		{
+			type: 'input',
+			name: 'themeSlug',
+			default: ( answers ) => paramCase( answers.themeName ),
+			message: 'Enter the slug of the theme:',
+			validate: validateSlug,
+		},
+		{
+			type: 'input',
+			name: 'phpNamespace',
+			default: ( answers ) =>
+				'Required\\' + pascalCase( answers.themeSlug.replace( '-theme', '' ) ) + '\\Theme',
+			message: 'Enter the PHP namespace of the theme:',
+			validate: validatePHPNamespace,
+		},
+		{
+			type: 'input',
+			name: 'githubSlug',
+			default: ( answers ) => answers.themeSlug,
+			message: 'Enter the slug of the GitHub repo:',
+			validate: validateSlug,
+		},
+	] );
+
+	log();
+
+	const { isReady } = await inquirer.prompt( [
+		{
+			type: 'expand',
+			name: 'isReady',
+			default: 2, // default to help in order to avoid clicking straight through
+			choices: [
+				{ key: 'y', name: 'Yes', value: true },
+				{ key: 'n', name: 'No', value: false },
+			],
+			message: 'Start rewriting the files for ' + themeName + ' with slug ' + themeSlug + '?',
+		},
+	] );
+
+	if ( ! isReady ) {
+		log( format.error( '\n\nAborting.' ) );
+		process.exit();
+	}
+
+	log();
+
+	await runStep( 'Renaming theme files', 'Could not rename files.', async () => {
+		const replacementOptions = {
+			allowEmptyPaths: true,
+			files: [
+				WORKING_DIR + '/README.md',
+				WORKING_DIR + '/composer.json',
+				WORKING_DIR + '/package.json',
+				WORKING_DIR + '/phpcs.xml.dist',
+				WORKING_DIR + '/style.css',
+				WORKING_DIR + '/*.php',
+				WORKING_DIR + '/inc/*.php',
+				WORKING_DIR + '/template-parts/*.php',
+			],
+			from: [
+				/Theme Name([^:])/g, // Ignore the colon so that in "Theme Name: Theme Name" only the second is replaced.
+				/Required\\ThemeName/g,
+				/Required\\\\ThemeName/g,
+				/theme-name/g,
+				/theme_name/g,
+				/ThemeName/g,
+				/Theme description\./g,
+				/wordpress-theme-boilerplate/g,
+			],
+			to: [
+				themeName + '$1',
+				phpNamespace,
+				phpNamespace.replace( /\\/g, '\\\\' ),
+				themeSlug,
+				snakeCase( themeSlug ),
+				camelCase( themeSlug ),
+				themeDescription,
+				githubSlug,
+			],
+		};
+
+		await replace( replacementOptions );
+	} );
+
+	log( format.success( '\n✅  Done!' ) );
+}
+
+module.exports = replaceFiles;
