66708af1ed73993e35b436c8e49d6b821eeee69a
Dominik Schilling <dominikschilling+git@gmail.com>
2021-06-01 08:41:30 +0200

    Remove development environment and use the browsers query of the production environment by default. (#60)

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,13 +1,10 @@
-module.exports = {
-	development: [ 'last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions' ],
-	production: [
-		'ie >= 11',
-		'last 2 ChromeAndroid versions',
-		'last 2 Chrome versions',
-		'last 2 Firefox versions',
-		'last 2 Safari versions',
-		'last 2 iOS versions',
-		'last 2 Edge versions',
-		'last 2 Opera versions',
-	],
-};
+module.exports = [
+	'ie >= 11',
+	'last 2 ChromeAndroid versions',
+	'last 2 Chrome versions',
+	'last 2 Firefox versions',
+	'last 2 Safari versions',
+	'last 2 iOS versions',
+	'last 2 Edge versions',
+	'last 2 Opera versions',
+];
