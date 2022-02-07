6d86e43086f72c439304cc03f0f7ebd1ff4bea15
Dominik Schilling <dominikschilling+git@gmail.com>
2022-02-07 18:15:03 +0100

    Add PostCSS test for modern browserslist config (#106)

--- packages/postcss-config/index.test.js
+++ packages/postcss-config/index.test.js
@@ -3,6 +3,19 @@ const postcss = require( 'postcss' );
 const postcssrc = require( 'postcss-load-config' );
 
 describe( 'postcss-config', () => {
+	const originalEnv = process.env;
+
+	beforeEach( () => {
+		jest.resetModules();
+		process.env = {
+			...originalEnv,
+		};
+	} );
+
+	afterEach( () => {
+		process.env = originalEnv;
+	} );
+
 	test( 'integration', () => {
 		const config = postcssrc.sync( {}, './' );
 
@@ -21,11 +34,49 @@ describe( 'postcss-config', () => {
 			} );
 	} );
 
+	test( 'integration modern browsers', () => {
+		process.env.BROWSERSLIST = 'extends @wearerequired/browserslist-config/modern';
+		const config = postcssrc.sync( {}, './' );
+
+		const input = readFileSync( './test/integration/fixtures/index.css', 'utf8' );
+		const output = readFileSync( './test/integration/expected/index.modern.css', 'utf8' );
+
+		return postcss( config.plugins )
+			.process( input, {
+				...config.options,
+				from: './test/integration/fixtures/index.css',
+				map: false, // Disable source maps for easier expectation.
+			} )
+			.then( ( result ) => {
+				expect( result.css ).toEqual( output );
+				expect( result.warnings() ).toHaveLength( 0 );
+			} );
+	} );
+
 	test( 'integration for production', () => {
 		const config = postcssrc.sync( { env: 'production' }, './' );
 
 		const input = readFileSync( './test/integration/fixtures/index.css', 'utf8' );
 		const output = readFileSync( './test/integration/expected/index.minified.css', 'utf8' );
+
+		return postcss( config.plugins )
+			.process( input, {
+				...config.options,
+				from: './test/integration/fixtures/index.css',
+				map: false, // Disable source maps for easier expectation.
+			} )
+			.then( ( result ) => {
+				expect( result.css ).toEqual( output );
+				expect( result.warnings() ).toHaveLength( 0 );
+			} );
+	} );
+
+	test( 'integration for production modern browsers', () => {
+		process.env.BROWSERSLIST = 'extends @wearerequired/browserslist-config/modern';
+		const config = postcssrc.sync( { env: 'production' }, './' );
+
+		const input = readFileSync( './test/integration/fixtures/index.css', 'utf8' );
+		const output = readFileSync( './test/integration/expected/index.modern.minified.css', 'utf8' );
 
 		return postcss( config.plugins )
 			.process( input, {
