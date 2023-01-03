c0d2e7d1a96c9bbb6d8c177b00eca6a3aac36c09
Dominik Schilling <dominikschilling+git@gmail.com>
2023-01-03 18:31:02 +0100

    Add support for linting test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
+	root: true,
 	extends: [ '@wearerequired/eslint-config' ],
+	overrides: [
+		{
+			files: [ '**/?(*.)test.js' ],
+			extends: [ 'plugin:@wordpress/eslint-plugin/test-unit' ],
+		},
+	],
+	settings: {
+		jest: {
+			version: 28
+		}
+	}
 };
